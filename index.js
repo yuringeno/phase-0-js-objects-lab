e3776dcda5e155a5ce1859a490b59d6a446de936
yuringeno <nicolecheptoo152@gmail.com>
2026-03-23 13:28:15 +0300

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 //Write your code here
 
+let attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP", 
+  ticketPrice: 150,
+  checkedIn: true
+};
+
+
+function logAttendeeName(attendeeObj) {
+  console.log(attendeeObj.name);
+}
+
+
+function logTicketPrice(attendeeObj) {
+  console.log(attendeeObj.ticketPrice);
+}
+
+
+function updateTicketType(attendeeObj, newType) {
+  attendeeObj.ticketType = newType;
+}
+
+
+function updateTicketPrice(attendeeObj, newPrice) {
+  attendeeObj.ticketPrice = newPrice;}
+
+function removeEventProperty(attendeeObj) {
+  delete attendeeObj.event;
+} 
+
+function addCheckedInProperty(attendeeObj) {
+  attendeeObj.checkedIn = true;
+} 
+
+
+
+
+
+
 
 
 //Needed for the tests to work. Don't modify
